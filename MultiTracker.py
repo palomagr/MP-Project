485a7a3fc2288d317db42213ee83c244e7ac5b5a
kmcpher <66139733+kmcpher@users.noreply.github.com>
2020-07-15 21:02:38 -0700

    Update timestamp, reselect boxes

--- MultiTracker.py
+++ MultiTracker.py
@@ -9,10 +9,11 @@ import cv2
 from random import randint
 import math
 from scipy import special
+import time as tm
 
 trackerTypes = ['BOOSTING', 'MIL', 'KCF','TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
 bboxes = []
-colors = []
+colors = {0:(0,0,255), 1:(0,128,255), 2:(0,255,255), 3:(0,255,0), 4:(255,0,0), 5:(255,0,128)}
 
 def createTrackerByName(trackerType):
   # Create a tracker based on tracker name
@@ -45,15 +46,12 @@ def create_boxes(): # helper method - select boxes
     ## Select boxes
     global bboxes
     bboxes = []
-    global colors
-    colors = []
     while True:
         # draw bounding boxes over objects
         # selectROI's default behaviour is to draw box starting from the center
         # when fromCenter is set to false, you can draw box starting from top left corner
         bbox = cv2.selectROI('Select Boxes', frame)
         bboxes.append(bbox)
-        colors.append((randint(64, 255), randint(64, 255), randint(64, 255)))
         print("Press q to quit selecting boxes and start tracking")
         print("Press any other key to select next object")
         if cv2.waitKey(0) & 0xFF == ord('q'):  # q is pressed
@@ -70,18 +68,19 @@ if __name__ == '__main__':
     trackerType = "CSRT"
 
     # Set video to load
-    videoPath = '/Users/kmcpherson/Documents/UROP/Videos/dj08_0.mp4'
+    videoPath = '/Users/kmcpherson/Documents/UROP/Videos/dj08/dj08_0.mp4'
     text_string = 'dj08_0.txt'
 
     # Create a video capture object to read videos
     cap = cv2.VideoCapture(videoPath)
     fps = cap.get(cv2.CAP_PROP_FPS)
-    time = '0'
+    time = cap.get(cv2.CAP_PROP_POS_MSEC)/1000
 
     # Read first frame
     cx, cy, user = 0,0,0
     success, frame = cap.read()
     width, height, channels = frame.shape
+    print(width, height)
 
     if not success: # quit if unable to read the video file
         print('Failed to read video')
@@ -105,28 +104,28 @@ if __name__ == '__main__':
     while cap.isOpened(): # Process video and track objectspremie
         success, frame = cap.read()
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(frame,str("{:.{}f}".format(float(time), 3)),(1070,60),font,1,(255,255,255),2,cv2.LINE_AA) #timestamp
+        time_mins_secs = tm.strftime('%M:%S', tm.gmtime((time))) #time (min:sec)
+        cv2.putText(frame,time_mins_secs,(1070,60),font,1,(255,255,255),2,cv2.LINE_AA) #timestamp
         if not success:
             print("Failed")
             break
 
         # get updated location of objects in subsequent frames
         success, boxes = multiTracker.update(frame)
-        users = {}
 
         for user, newbox in enumerate(boxes):
             x, y = int(newbox[0]), int(newbox[1]) # starting point
-            wid, len = rwid[user], rlen[user]
-            cv2.rectangle(frame, (x,y), (x+wid, y+len), colors[user], 2, 1)
-            cx = (newbox[0]) + (wid/2) * (900/width) #midpoint of the selected region in x
-            cy = (newbox[1]) + (len/2) * (900/height) #midpoint of the selecteed region in y
+            wid, len = rwid[user], rlen[user] #width and length of the selected rectangle
+            cv2.rectangle(frame, (x,y), (x+wid, y+len), colors[user], 2, 1) #draws a rectangle around the object
+            cx = ((newbox[0]) + (wid/2)) * (900/width) #midpoint of the selected region in x (image is 900 pixels)
+            cy = ((newbox[1]) + (len/2)) * (900/height) #midpoint of the selecteed region in y
             time = cap.get(cv2.CAP_PROP_POS_MSEC)/1000
 
 
             if time%1 < 0.042: #every second
                 f.write(str(user) +'\t'+ str("{:.{}f}".format(cx, 3))
                                +'\t'+ str("{:.{}f}".format(cy, 3))
-                               +'\t'+ str(float(int(time))) +'\n') #write data
+                               +'\t'+ time_mins_secs +'\n') #write data
 
         # show frame / write data
         cv2.imshow('Tracking Trajectories', frame)
