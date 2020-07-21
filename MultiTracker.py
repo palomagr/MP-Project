3b6d3a04c5daeaf498fc31b10c3a443a38101e1c
kmcpher <66139733+kmcpher@users.noreply.github.com>
2020-07-21 14:45:39 -0700

    Comments
    Comments added to explain the code and show which code I added

--- MultiTracker.py
+++ MultiTracker.py
@@ -3,17 +3,18 @@
 #
 # Copyright 2018 BIG VISION LLC ALL RIGHTS RESERVED
 #
+# /// = code by Kimmy
 from __future__ import print_function
 import sys
 import cv2
 from random import randint
 import math
 from scipy import special
-import time as tm
+import time as tm #///
 
 trackerTypes = ['BOOSTING', 'MIL', 'KCF','TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
 bboxes = []
-colors = {0:(0,0,255), 1:(0,128,255), 2:(0,255,255), 3:(0,255,0), 4:(255,0,0), 5:(255,0,128)}
+colors = {0:(0,0,255), 1:(0,128,255), 2:(0,255,255), 3:(0,255,0), 4:(255,0,0), 5:(255,0,128)} #/// makes dict of colors - first person red, second orange, ...
 
 def createTrackerByName(trackerType):
   # Create a tracker based on tracker name
@@ -42,8 +43,7 @@ def createTrackerByName(trackerType):
 
     return tracker
 
-def create_boxes(): # helper method - select boxes
-    ## Select boxes
+def create_boxes(): #/// helper method - select boxes
     global bboxes
     bboxes = []
     while True:
@@ -54,9 +54,14 @@ def create_boxes(): # helper method - select boxes
         bboxes.append(bbox)
         print("Press q to quit selecting boxes and start tracking")
         print("Press any other key to select next object")
-        if cv2.waitKey(0) & 0xFF == ord('q'):  # q is pressed
+        if cv2.waitKey(0) & 0xFF == ord('q'):  # q is pressed (quit)
             print("q pressed")
             break
+    i = 0 #/// this section updates rectangle sizes when you reselect
+    for bbox in bboxes:
+        rwid[i] = bbox[2]
+        rlen[i] = bbox[3]
+        i += 1
 
 
 if __name__ == '__main__':
@@ -68,8 +73,9 @@ if __name__ == '__main__':
     trackerType = "CSRT"
 
     # Set video to load
-    videoPath = '/Users/kmcpherson/Documents/UROP/Videos/dj08/dj08_0.mp4'
-    text_string = 'dj08_0.txt'
+    videoPath = '/Users/kmcpherson/Documents/UROP/Videos/dj08/dj08_2.mp4'
+    text_string = 'dj08_2.txt' # full file - records every frame
+    sim_text_string = 'dj08_2_sim.txt' #/// simplified file - records every second
 
     # Create a video capture object to read videos
     cap = cv2.VideoCapture(videoPath)
@@ -78,35 +84,38 @@ if __name__ == '__main__':
 
     # Read first frame
     cx, cy, user = 0,0,0
-    success, frame = cap.read()
-    width, height, channels = frame.shape
-    print(width, height)
+    success, frame = cap.read() # read the first frame
+    width, height, channels = frame.shape #/// get width and height
 
     if not success: # quit if unable to read the video file
         print('Failed to read video')
         sys.exit(1)
 
-    create_boxes()
-    print('Selected bounding boxes {}'.format(bboxes))
+    rwid = {} #/// width of the rect. drawn for each person
+    rlen = {} #/// length of the rect. drawn for each person
 
-    rwid= {} #width of the rectangle drawn for each person
-    rlen = {} #length of the rect. drawn for each person
+    create_boxes() #/// helper method
+    #print('Selected bounding boxes {}'.format(bboxes)) # check the boxes you selected
+
     multiTracker = cv2.MultiTracker_create() # Create MultiTracker object
     i = 0
-    for bbox in bboxes: # Initialize MultiTracker
+    for bbox in bboxes: #/// make the rectangles for each person
         rwid[i] = bbox[2]
         rlen[i] = bbox[3]
         i += 1
         multiTracker.add(createTrackerByName(trackerType), frame, bbox)
 
-    f = open(text_string, "w") #open the file to write to
+    f = open(text_string, "w") # open full file
+    fsim = open(sim_text_string, "w") #/// open simplified file
+
+    print('Press X to reselect boxes')
 
     while cap.isOpened(): # Process video and track objectspremie
         success, frame = cap.read()
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        time_mins_secs = tm.strftime('%M:%S', tm.gmtime((time))) #time (min:sec)
-        cv2.putText(frame,time_mins_secs,(1070,60),font,1,(255,255,255),2,cv2.LINE_AA) #timestamp
-        if not success:
+        font = cv2.FONT_HERSHEY_SIMPLEX #///
+        time_mins_secs = tm.strftime('%M:%S', tm.gmtime((time))) #/// time (min:sec)
+        cv2.putText(frame,time_mins_secs,(int(.85*width),int(.05*height)),font,1,(255,255,255),2,cv2.LINE_AA) #/// timestamp, upper right
+        if not success: # if video ends or can't be read
             print("Failed")
             break
 
@@ -114,32 +123,36 @@ if __name__ == '__main__':
         success, boxes = multiTracker.update(frame)
 
         for user, newbox in enumerate(boxes):
-            x, y = int(newbox[0]), int(newbox[1]) # starting point
-            wid, len = rwid[user], rlen[user] #width and length of the selected rectangle
-            cv2.rectangle(frame, (x,y), (x+wid, y+len), colors[user], 2, 1) #draws a rectangle around the object
-            cx = ((newbox[0]) + (wid/2)) * (900/width) #midpoint of the selected region in x (image is 900 pixels)
-            cy = ((newbox[1]) + (len/2)) * (900/height) #midpoint of the selecteed region in y
+            x, y = int(newbox[0]), int(newbox[1]) #/// starting point
+            wid, len = rwid[user], rlen[user] #/// width and length of the selected rectangle
+            cv2.rectangle(frame, (x,y), (x+wid, y+len), colors[user], 2, 1) #/// draws a rectangle around the object
+            cx = ((newbox[0]) + (wid/2)) * (900/width) #/// midpoint of the selected region in x (image is 900 pixels)
+            cy = ((newbox[1]) + (len/2)) * (900/height) #/// midpoint of the selecteed region in y
             time = cap.get(cv2.CAP_PROP_POS_MSEC)/1000
 
+            f.write(str(user) +'\t'+ # unsimplified file
+                    str("{:.{}f}".format(cx, 3)) +'\t'+
+                    str("{:.{}f}".format(cy, 3)) +'\t'+
+                    str("{:.{}f}".format(time, 3)) +'\n')
 
-            if time%1 < 0.042: #every second
-                f.write(str(user) +'\t'+ str("{:.{}f}".format(cx, 3))
-                               +'\t'+ str("{:.{}f}".format(cy, 3))
-                               +'\t'+ time_mins_secs +'\n') #write data
+            if time%1 <= (1/fps): #/// simplified file (every second)
+                fsim.write(str(user) +'\t'+
+                           str("{:.{}f}".format(cx, 3))+'\t'+
+                           str("{:.{}f}".format(cy, 3))+'\t'+
+                           str(float(int(time))) +'\n') #write data
 
-        # show frame / write data
+        # track trajectories / write data
         cv2.imshow('Tracking Trajectories', frame)
 
-        #spacebar = 32
-        if cv2.waitKey(1) & 0xFF == ord('x'): # X pressed
+        if cv2.waitKey(1) & 0xFF == ord('x'): #/// X pressed - reselect boxes
             create_boxes()
-            print('Selected bounding boxes {}'.format(bboxes))
-            multiTracker = cv2.MultiTracker_create() # Create MultiTracker object
-            for bbox in bboxes: # Initialize MultiTracker
+            multiTracker = cv2.MultiTracker_create()
+            for bbox in bboxes:
                 multiTracker.add(createTrackerByName(trackerType), frame, bbox)
 
         # quit on ESC button
         elif cv2.waitKey(1) & 0xFF == 27:  # Esc pressed
             break
 
-    f.close()
+    f.close() # close the files at the end
+    fsim.close()
