474787c9aa08a2841381e002516aedda2a52c9b9
kmcpher <66139733+kmcpher@users.noreply.github.com>
2020-12-22 15:16:25 -0800

    final touch-ups

--- readData.py
+++ readData.py
@@ -1,4 +1,6 @@
 
+""" NOTICE: the filepaths will need to be changed since this was for my computer """
+
 import numpy as np
 import math
 import pandas as pd
@@ -6,11 +8,11 @@ import os
 import cv2
 from PIL import Image
 
-filepath = '/Users/kmcpherson/Documents/UROP/' #filepath to the main folder - can be changed depending on the user
-testfile = filepath + 'testfile.txt' #tester file with more indices than just 0 & 1
+filepath = '/Users/kmcpherson/Documents/UROP/' # filepath to the main folder - can be changed depending on the user
+testfile = filepath + 'testfile.txt' # tester file with more indices than just 0 & 1
 datafile = filepath + 'Videos/Video Text Files/dj01/g01_2.txt'
-col_coord = ['Visitor Index', 'X coordinate', 'Y coordinate', 'Time'] #names of the columns for the coordinate file
-col_dir = ['Visitor Index', 'Action', 'Distance', 'Timestamp', 'Time Static'] #names of the columns for the directions file
+col_coord = ['Visitor Index', 'X coordinate', 'Y coordinate', 'Time'] # names of the columns for the coordinate file
+col_dir = ['Visitor Index', 'Action', 'Distance', 'Timestamp', 'Time Static'] # names of the columns for the directions file
 file_list = []
 
 BLUE = (3, 155, 229)
@@ -22,7 +24,7 @@ RED = (255, 0, 0)
 heatmap_dict = {}
 for x in range(900):
     for y in range(900):
-        heatmap_dict[(x,y)] = -1
+        heatmap_dict[(x,y)] = -1 # start every value off as blank (-1)
 
 heat_color = {BLUE: [],
               GREEN: [],
@@ -35,15 +37,15 @@ def separate_file(filename):
     """
     Separates one .txt file into individual .txt files for each index
     """
-    file = open(filename, 'r') #whole .txt file with indices
+    file = open(filename, 'r') # whole .txt file with indices
     lines = file.readlines()
-    index_dict = {} #dictionary that maps index to filepath
-    for line in lines: #finds the indices, fills the dictionary
-        index = line.split()[0] + 'file' #0file, 1file, ...
-        if index not in index_dict: #if it's a new index
-            file_list.append(filename.replace('.txt', '__'+index+'.txt')) #adds the file to the global list
-            index_dict[index] = filename.replace('.txt', '__'+index+'.txt') #make a key in the dict for it
-            globals()[index] = open(index_dict[index], 'w') #opens each of the files and assigns them to a variable (0file = __, 1file = __)
+    index_dict = {} # dictionary that maps index to filepath
+    for line in lines: # finds the indices, fills the dictionary
+        index = line.split()[0] + 'file' # 0file, 1file, ...
+        if index not in index_dict: # if it's a new index
+            file_list.append(filename.replace('.txt', '__'+index+'.txt')) # adds the file to the global list
+            index_dict[index] = filename.replace('.txt', '__'+index+'.txt') # make a key in the dict for it
+            globals()[index] = open(index_dict[index], 'w') # opens each of the files and assigns them to a variable (0file = __, 1file = __)
         globals()[index].write(line)
 
 
@@ -51,14 +53,14 @@ def txt_to_csv(filename, cols):
     """
     Converts a .txt file to .csv
     """
-    f = open(filename, 'r') #open the file
-    if 'dir' in cols: #create the columns
+    f = open(filename, 'r') # open the file
+    if 'dir' in cols: # create the columns
         columns = col_dir
     else:
         columns = col_coord
 
-    read_file = pd.read_csv(filename, delimiter='\t', header=None, names=columns) #take in the .txt file
-    read_file.to_csv(filename.replace('txt', 'csv'), index=None) #convert to .csv
+    read_file = pd.read_csv(filename, delimiter='\t', header=None, names=columns) # take in the .txt file
+    read_file.to_csv(filename.replace('txt', 'csv'), index=None) # convert to .csv
 
 
 def simplify(filename):
@@ -71,60 +73,62 @@ def simplify(filename):
     desired_lines = []
     seconds_list = []
     for line in lines:
-        split = line.split() #[index, x coord, y coord, time]
-        if (int(float(split[3]))) not in seconds_list: #simplifies it to every second
+        split = line.split() # [index, x coord, y coord, time]
+        if (int(float(split[3]))) not in seconds_list: # simplifies it to every second
             seconds_list.append(int(float(split[3])))
             desired_lines.append(line)
-    file = open(filename.replace('.txt', '_sim.txt'), 'w') #create simplified file
-    for line in desired_lines: #print every 24th line (every second)
-        file.write(line[:len(line)-4:] + '0\n') #simplifies the seconds (0.0, 1.0, 2.0 ...)
+    file = open(filename.replace('.txt', '_sim.txt'), 'w') # create simplified file
+    for line in desired_lines: # print every 24th line (every second)
+        file.write(line[:len(line)-4:] + '0\n') # simplifies the seconds (0.0, 1.0, 2.0 ...)
 
 
 def get_directions(filename, numDirections):
     """
     Gets list (.txt) of directions from .txt file
     """
-    file = open(filename, 'r') #.txt file
-    dir_file = open(filename.replace('.txt', '_dir.txt'), 'w') #file with directions
+    file = open(filename, 'r') # .txt file
+    dir_file = open(filename.replace('.txt', '_dir.txt'), 'w') # file with directions
 
-    interval = 360.0/numDirections #the range of each direction
+    interval = 360.0/numDirections # the range of each direction
 
-    directions_dict = {} #dictionary of directions: [0-45:D1], [45-90:D2], [90-135:D3] ...
+    directions_dict = {} # dictionary of directions: [0-45:D1], [45-90:D2], [90-135:D3] ...
     for x in range(numDirections):
-        directions_dict[(interval*x, interval*(x+1))] = ('D' + str(x+1)) #maps 'D1' to a range of degrees, 'D2' to the next range, etc.
+        directions_dict[(interval*x, interval*(x+1))] = ('D' + str(x+1)) # maps 'D1' to a range of degrees, 'D2' to the next range, etc.
 
-    dir_list = [] #list of directions to see if it matches the previous line (in that case, just go forward)
+    # first loop: sets the direction for each data coordinate
+    dir_list = [] # list of directions to see if it matches the previous line (in that case, just go forward)
     lines = file.readlines()
-    for x in range(len(lines)-1): #for every line (except last)
+    for x in range(len(lines)-1): # for every line (except last)
         thisLine = lines[x].split()
-        newLine = thisLine[0]
-        nextLine = lines[x+1].split() #looks at next coords to get vector direction
-        Ydiff = float(nextLine[2]) - float(thisLine[2]) #Y component
-        Xdiff = float(nextLine[1]) - float(thisLine[1]) #X component
-        distance = math.sqrt(Ydiff**2 + Xdiff**2)*(4/3) #distance the person moves (4/3 b/c it's out of 900 & it should be 1200)
-        degrees = round((math.atan2(Xdiff, Ydiff)/math.pi*180), 1) #direction in degrees the person is moving at that second
-        if degrees < 0: #accounts for negative degrees - makes the range 0 to 360 instead of -180 to 180
-            degrees += 360
+        nextLine = lines[x+1].split() # looks at next coords to get vector direction
+        Ydiff = float(nextLine[2]) - float(thisLine[2]) # Y component
+        Xdiff = float(nextLine[1]) - float(thisLine[1]) # X component
+        distance = math.sqrt(Ydiff**2 + Xdiff**2)*(4/3) # distance the person moves (4/3 b/c it's out of 900 & it should be 1200)
+        degrees = round((math.atan2(Xdiff, Ydiff)/math.pi*180), 1) # direction in degrees the person is moving at that second
 
+        if degrees < 0: # accounts for negative degrees - makes the range 0 to 360 instead of -180 to 180
+            degrees += 360
         for key in directions_dict:
-            if degrees == 0 or distance < .25: #if the person doesn't move
+            if degrees == 0 or distance < .25: # if the person doesn't move
                 dir_list.append('None')
                 break
-            elif key[0] < degrees <= key[1]: #if the person's direction falls within a D#'s range
+            elif key[0] < degrees <= key[1]: # if the person's direction falls within a D#'s range
                 dir_list.append(directions_dict[key])
                 break
 
+    # second loop: interprets that data to write to the file
     timeDiff, timePrint = 0,0
-    for x in range(len(lines)-2): #for every line (except last)
+    for x in range(len(lines)-2): # for every line (except last)
         thisLine = lines[x].split()
-        newLine = thisLine[0]
-        nextLine = lines[x+1].split() #looks at next coords to get vector direction
-        Ydiff = float(nextLine[2]) - float(thisLine[2]) #Y component
-        Xdiff = float(nextLine[1]) - float(thisLine[1]) #X component
+        nextLine = lines[x+1].split() # looks at next coords to get vector direction
+        Ydiff = float(nextLine[2]) - float(thisLine[2]) # Y component
+        Xdiff = float(nextLine[1]) - float(thisLine[1]) # X component
         distance = math.sqrt(Ydiff**2 + Xdiff**2)*(4/3) # ** distance to use in the future for the unity simulation (meters)
-        degrees = round((math.atan2(Xdiff, Ydiff)/math.pi*180), 1) #direction in degrees the person is moving at that second
+        newLine = thisLine[0] # the new line that we'll print - for now, it's just the index
         time_interval = float(nextLine[3]) - float(thisLine[3])
 
+        # exceptions - after shifting coords, somtimes they get out of the (0, 899) range
+        # this stuff puts them back within those bounds
         this_point = (int(float(thisLine[1])), int(float(thisLine[2])))
         if this_point[0] < 0:
             this_point = (0, this_point[1])
@@ -135,52 +139,49 @@ def get_directions(filename, numDirections):
         elif this_point[1] > 899:
             this_point = (this_point[0], 899)
 
-        if dir_list[x] == 'None': #if this line has no direction (doesn't move)
-            newLine += '\tNone\t' #give it no direction
+        if dir_list[x] == 'None': # if this line has no direction (doesn't move)
+            newLine += '\tNone\t' # give it no direction
             distance = 0
-            #time static column
-            if dir_list[x-1] != 'None': #if this is the first 'None' (the previous one is not None)
-                timeDiff = time_interval #start keeping track of time
+
+            # time static column - records how long the person stayed there
+            if dir_list[x-1] != 'None': # if this is the first 'None' (the previous one is not None)
+                timeDiff = time_interval # start keeping track of time
                 timePrint = 0.0
-                if dir_list[x+1] != 'None': #if it's the first and last None (only 1 second)
-                    timePrint = timeDiff #record this time to print
-
+                if dir_list[x+1] != 'None': # if it's the first and last None (only 1 second)
+                    timePrint = timeDiff # record this time to print
                     if heatmap_dict[this_point] == -1:
                         heatmap_dict[this_point] = 0
                     else:
                         heatmap_dict[this_point] += timePrint
-
-                    timeDiff = 0.0
-            else: #if there was a 'None' before this
-                timeDiff += time_interval #add the line's time
-                if dir_list[x+1] != 'None': #if this is the last 'None'
+                    timeDiff = 0.0 # reset the time
+            else: # if there was a 'None' before this
+                timeDiff += time_interval # add the line's time
+                if dir_list[x+1] != 'None': # if this is the last 'None'
                     timePrint = timeDiff
-
                     if heatmap_dict[this_point] == -1:
                         heatmap_dict[this_point] = 0
                     else:
                         heatmap_dict[this_point] += timePrint
+                    timeDiff = 0.0 # reset the time
 
-                    timeDiff = 0.0 #reset the time
-        else: #if it has a direction
+        else: # if it has a direction
             timePrint = 0.0
-
             if heatmap_dict[this_point] == -1:
                 heatmap_dict[this_point] = 0.0
             else:
                 heatmap_dict[this_point] += time_interval
-
-            if dir_list[x] == dir_list[x-1]: #if this direction is the same as the previous line
-                newLine += '\t' + 'F\t' #just go forward
+            if dir_list[x] == dir_list[x-1]: # if this direction is the same as the previous line
+                newLine += '\t' + 'F\t' # just go forward
             else:
-                newLine += '\t' + dir_list[x] + ', F\t' #turn and go forward
+                newLine += '\t' + dir_list[x] + ', F\t' # turn and go forward
 
         if timePrint == 0:
             newLine += str(round(distance,3)) + '\t' +(thisLine[3] + '\t' + '' + '\n')
         else:
-            newLine += str(round(distance,3)) + '\t' +(thisLine[3] + '\t' + str("{:.{}f}".format(timePrint, 2)) + '\n') #distance, time, time static columns
+            newLine += str(round(distance,3)) + '\t' +(thisLine[3] + '\t' + str("{:.{}f}".format(timePrint, 2)) + '\n') # distance, time, time static columns
 
-        dir_file.write(newLine) #writes the line in the .txt file
+        dir_file.write(newLine) # writes the line in the .txt file
+        
     old_filename = filename.replace('.txt', '_dir.txt')
     new_filename = filepath + 'Videos/Video Text Files/Directions/' + os.path.basename(old_filename)
     os.rename(old_filename, new_filename)
@@ -367,7 +368,6 @@ def load_image(filename):
         w, h = img.size
         return {'height': h, 'width': w, 'pixels': pixels}
 
-
 def save_image(image, filename, mode='PNG'):
     """
     Saves the given color image to disk or to a file-like object.  If filename
@@ -383,7 +383,6 @@ def save_image(image, filename, mode='PNG'):
         out.save(filename, mode)
     out.close()
 
-
 def get_pixel(image, x, y):
     width = image['width']
     height = image['height']
@@ -398,7 +397,6 @@ def get_pixel(image, x, y):
             return get_pixel(image, x, 0) # return the pixel at the top of the column
         elif y > (height-1): # below the image
             return get_pixel(image, x, height-1) # return the pixel at the bottom of the column
-
 
 def set_pixel(image, x, y, c):
     image['pixels'][image['width']*y + x] = c
@@ -427,15 +425,8 @@ if __name__ == '__main__':
     heatmap_file.close()
 
     # draw the heatmap and save the file
-    #create_heatmap('total_heatmap.png') # ! has to be done after get_directions !
+    create_heatmap('total_heatmap.png') # ! has to be done after get_directions !
 
     # a ton of files are made in the process - delete them at the end
     for file in file_list:
         os.remove(file)
-
-
-
-
-
-
-    #
